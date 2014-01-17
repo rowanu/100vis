d97681e60bb84a931c72294f93586748c1164388
rowan <rowanu@gmail.com>
2014-01-17 13:38:49 -0300

    Use live data for day002

--- js/002.js
+++ js/002.js
@@ -60,8 +60,7 @@ function render(data, selector) {
     .attr('d', line);
 }
 
-// d3.csv('https://api.bitcoinaverage.com/history/USD/per_minute_24h_sliding_window.csv', function (err, data) {
-d3.csv('/per_minute_24h_sliding_window.csv', function (err, data) {
+d3.csv('https://api.bitcoinaverage.com/history/USD/per_minute_24h_sliding_window.csv', function (err, data) {
   if (err) { console.error(err); }
   render(data, '#chart');
 });
