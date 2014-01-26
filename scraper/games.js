786419fd9c04a5e535b3d69642de56529f869a3b
rowan <rowanu@gmail.com>
2014-01-26 20:25:28 -0300

    Created scraper for cf open

--- scraper/games.js
+++ scraper/games.js
@@ -1,23 +1,42 @@
 /*globals pjs, $ */
 pjs.config({ 
-  // options: 'stdout', 'file' (set in config.logFile) or 'none'
   log: 'stdout',
-  // options: 'json' or 'csv'
   format: 'json',
-  // options: 'stdout' or 'file' (set in config.outFile)
-  writer: 'stdout',
-  // writer: 'file',
-  outFile: 'data/games.json'
+  // writer: 'stdout',
+  writer: 'file',
+  outFile: 'data/2013-crossfit-open-women.json'
 });
 
 pjs.addSuite({
-    url: 'http://games.crossfit.com/scores/leaderboard.php?stage=5&sort=0&division=2&region=4&numberperpage=100&page=0&competition=0&frontpage=1&expanded=1&full=0&year=13&showtoggles=0&hidedropdowns=0&showathleteac=0&athletename=',
+    url: 'http://games.crossfit.com/scores/leaderboard.php?' +
+      'stage=5&' +
+      'sort=0&' +
+      'division=2&' +
+      'region=4&' +
+      'numberperpage=100&' +
+      'page=0&' +
+      'competition=0&' +
+      'frontpage=1&' +
+      'expanded=1&' +
+      'full=0&' +
+      'year=13&' +
+      'showtoggles=0&' +
+      'hidedropdowns=0&' +
+      'showathleteac=0&' +
+      'athletename=',
     scraper: function () {
-      var athlete, athletes = [];
+      var athlete, athleteDOM, athletes = [];
 
-      $('.leaderboard-box tr[class!="lbhead"]').each(function () {
+      $('.leaderboard-box tr').each(function () {
+        athleteDOM = $(this).find('td');
         athlete = {
-          name: $(this).find('td.name').text()
+          rankScore: $(athleteDOM[0]).text().trim(),
+          name: $(athleteDOM[1]).text().trim(),
+          event01: $(athleteDOM[2]).text().trim(),
+          event02: $(athleteDOM[3]).text().trim(),
+          event03: $(athleteDOM[4]).text().trim(),
+          event04: $(athleteDOM[5]).text().trim(),
+          event05: $(athleteDOM[6]).text().trim(),
         };
         // Check required fields are present
         if (athlete.name) {
@@ -27,8 +46,5 @@ pjs.addSuite({
       return athletes;
     },
     moreUrls: '#leaderboard-pager a',
-    // moreUrls: function () {
-    //   return _pjs.getAnchorUrls('#leaderboard-pager a');
-    // },
     maxDepth: 1
 });
